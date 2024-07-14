581cc10dd7bff9e8f589b44754026a1f241a4de3
Vinayak Jadhav <vijadhav88@gmail.com>
2024-07-14 21:01:17 +0530

    first commit

---
+++ src/Automation/JASON.java
@@ -0,0 +1,14 @@
+package Automation;
+
+public class JASON {
+
+	public JASON() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
